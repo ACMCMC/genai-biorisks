a0cf43cc343b91b5cb96203d8c8ce407652a62bc
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2025-08-23 02:15:10 -0700

    Final Experimental Pipeline

--- run_full_pipeline.sh
+++ run_full_pipeline.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# GenAI Biorisks Full Experimental Pipeline
-
-nvidia-smi
 
 /opt/conda/bin/conda run --live-stream --prefix /home/acreomarino/genai-biorisks/.conda python run_experiment.py ${1:-small}
-# /opt/conda/bin/conda run --live-stream --prefix /home/acreomarino/genai-biorisks/.conda python run_experiment.py ${1:-medium}
 /opt/conda/bin/conda run --live-stream --prefix /home/acreomarino/genai-biorisks/.conda python run_experiment.py ${1:-full}
